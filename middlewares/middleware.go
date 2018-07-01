a9e49825812b24f25c7cf4b3187f40e715271f83
Pavle Lee <523260513@qq.com>
2018-07-01 20:04:13 +0800

    init project

--- middlewares/middleware.go
+++ middlewares/middleware.go
@@ -12,7 +12,7 @@ func RegisterMiddleware(router *gin.Engine) {
 	router.Use(gin.Recovery())
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:8010", "http://mc.liv1020.com"},
+		AllowAllOrigins:  true,
 		AllowMethods:     []string{"GET", "POST", "PUT", "HEAD", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
 		AllowCredentials: true,
